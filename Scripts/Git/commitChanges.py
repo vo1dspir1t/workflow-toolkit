1b4d3fb4969beccfd6f8cd157a3d1a44962f18a0
Danil <mikhov.danil@yandex.ru>
2024-03-20 11:41:43 +0300

    [WFT] Hotfix: Переработана дорожная карта

--- Scripts/Git/commitChanges.py
+++ Scripts/Git/commitChanges.py
@@ -3,11 +3,11 @@ from changeVersion import projectVersion
 
 types = ['Refactoring', 'Feature', 'Changes', 'Hotfix']
 
-changeType = projectVersion()
 print('\nПечатаем статус Git')
 os.system('git status')
 print('\nДобавляем все файлы для коммита\n')
 os.system('git add --all')
+changeType = projectVersion()
 os.system(f'git commit -m "[WFT] {types[changeType]}: {input("Сообщение коммита: ")}"')
 print('\nКоммит создан успешно.')
 os.system('pause')
