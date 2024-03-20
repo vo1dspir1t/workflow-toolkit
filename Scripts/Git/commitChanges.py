d0fdcc5f52d9d86fa9b5ee5e8266a03966849ba7
Danil <mikhov.danil@yandex.ru>
2024-03-20 11:43:12 +0300

    [WFT] Hotfix: Исправление версионирования проекта

--- Scripts/Git/commitChanges.py
+++ Scripts/Git/commitChanges.py
@@ -5,9 +5,9 @@ types = ['Refactoring', 'Feature', 'Changes', 'Hotfix']
 
 print('\nПечатаем статус Git')
 os.system('git status')
+changeType = projectVersion()
 print('\nДобавляем все файлы для коммита\n')
 os.system('git add --all')
-changeType = projectVersion()
 os.system(f'git commit -m "[WFT] {types[changeType]}: {input("Сообщение коммита: ")}"')
 print('\nКоммит создан успешно.')
 os.system('pause')
